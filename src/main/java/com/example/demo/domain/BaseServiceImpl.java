74bff007b79863d9d3660d2100e7a5de13bb649f
IghorBr <ighorbruno20@gmail.com>
2022-02-26 12:58:09 -0300

    acerto em BaseServiceImpl

--- src/main/java/com/example/demo/domain/BaseServiceImpl.java
+++ src/main/java/com/example/demo/domain/BaseServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class BaseServiceImpl<T extends BaseDomain> implements BaseService<T> {
+public abstract class BaseServiceImpl<T extends BaseDomain> implements BaseService<T> {
 
 	@Autowired
 	private BaseRepository<T> baseRepository;
